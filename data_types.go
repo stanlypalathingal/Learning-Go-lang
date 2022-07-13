31d8bea9820268f006aaabbea592bf257b3d9a0e
stanlypalathingal <b9058657@newcastle.ac.uk>
2022-07-13 16:32:20 +0100

    first push on Go-lang

---
+++ data_types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x float64 = 20.0
+
+	y := 45
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf(" x is of type %T \n", x)
+	fmt.Printf(" y is of type %T \n", y)
+	fmt.Printf(" x is of type %f \n", x)
+	fmt.Printf(" y is of type %d \n", y)
+}
